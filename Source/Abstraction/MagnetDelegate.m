38a7f77da8b9796fbb4975c214a1f9c5d209db6e
Jason Ferguson <jason.ferguson@magnet.com>
2015-08-18 13:24:23 -0700

    Added assertion to make sure a Configurations.plist is included in the project

--- Source/Abstraction/MagnetDelegate.m
+++ Source/Abstraction/MagnetDelegate.m
@@ -66,6 +66,9 @@ NSString  * const MMXMessageFailureBlockKey = @"MMXMessageFailureBlockKey";
 }
 
 - (void)startMMXClient {
+	//You must include your Configurations.plist file in the project. You can download this file on the Settings page of the Magnet Message Console
+	NSString *pathAndFileName = [[NSBundle mainBundle] pathForResource:@"Configurations" ofType:@"plist"];
+	NSAssert([[NSFileManager defaultManager] fileExistsAtPath:pathAndFileName], @"You must include your Configurations.plist file in the project. You can download this file on the Settings page of the Magnet Message Web Interface");
 	if ([MMXClient sharedClient].connectionStatus != MMXConnectionStatusAuthenticated &&
 		[MMXClient sharedClient].connectionStatus != MMXConnectionStatusConnected) {
 		MMXConfiguration * config = [MMXConfiguration configurationWithName:@"default"];
