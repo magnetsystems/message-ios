48836054dbce00a01db1e01d354cfca261369428
Jason Ferguson <jason.ferguson@magnet.com>
2015-08-21 10:26:22 -0700

    Reverting Reg change until we can make a more robust change next week

--- Source/User/MMXAccountManager.m
+++ Source/User/MMXAccountManager.m
@@ -155,7 +155,7 @@
 	
 	NSURLSession * session = [NSURLSession sessionWithConfiguration:config];
 	
-	NSString *usersURLString = [NSString stringWithFormat:@"https://%@:%li/mmxmgmt/api/v1/users",self.delegate.configuration.baseURL.host,(long)self.delegate.configuration.publicAPIPort + 1];
+	NSString *usersURLString = [NSString stringWithFormat:@"http://%@:%li/mmxmgmt/api/v1/users",self.delegate.configuration.baseURL.host,(long)self.delegate.configuration.publicAPIPort];
 	
 	NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:usersURLString]];
 	
