54f5618a069435b246b3db5d2423e97884252642
Jason Ferguson <jason.ferguson@magnet.com>
2015-10-28 18:22:49 -0700

    Fixed bug with sender user object for inbound messages

--- Source/CoreMessaging/MMXClient.m
+++ Source/CoreMessaging/MMXClient.m
@@ -920,20 +920,32 @@ int const kReconnectionTimerInterval = 4;
 												  from:(XMPPJID *)from
 													to:(XMPPJID *)to
 											 messageID:(NSString *)messageID {
-
-	NSArray *usernamesArray = [message.recipients valueForKey:@"username"];
+	
+	NSMutableArray *usernamesArray = [NSMutableArray arrayWithArray:[message.recipients valueForKey:@"username"]];
+	BOOL recipientsContainSender = NO;
+	if ([usernamesArray containsObject:message.senderUserID.username]) {
+		recipientsContainSender = YES;
+	} else {
+		[usernamesArray addObject:message.senderUserID.username];
+	}
+	[usernamesArray addObject:message.senderUserID.username];
 	[MMUser usersWithUserIDs:usernamesArray success:^(NSArray *users) {
-		MMXMessage *msg = [MMXMessage messageToRecipients:[NSSet setWithArray:users]
-										   messageContent:message.metaData];
-		
-		msg.messageType = MMXMessageTypeDefault;
-		
 		MMUser *sender;
+		NSMutableArray *usersCopy = users.mutableCopy;
 		for (MMUser *user in users) {
 			if ([user.userID.lowercaseString isEqualToString:message.senderUserID.username.lowercaseString]) {
 				sender = user.copy;
 			}
 		}
+		if (!recipientsContainSender) {
+			[usersCopy removeObject:sender];
+		}
+		
+		MMXMessage *msg = [MMXMessage messageToRecipients:[NSSet setWithArray:usersCopy]
+										   messageContent:message.metaData];
+		
+		msg.messageType = MMXMessageTypeDefault;
+		
 		msg.sender = sender;
 		msg.timestamp = message.timestamp;
 		msg.messageID = message.messageID;
@@ -947,7 +959,7 @@ int const kReconnectionTimerInterval = 4;
 	} failure:^(NSError * error) {
 		[[MMLogger sharedLogger] error:@"Failed to get users for Inbound Message\n%@",error];
 	}];
-
+	
 }
 
 - (void)handleInviteMessageFromInternalMessageAdaptor:(MMXInternalMessageAdaptor *)message
